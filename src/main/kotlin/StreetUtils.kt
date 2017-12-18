3db40181030466a03945452e8af5a5f446dae16a
javacook <javacook@gmx.de>
2017-12-18 22:12:57 +0100

    initial

---
+++ src/main/kotlin/StreetUtils.kt
@@ -0,0 +1,59 @@
+package de.kotlincook.textmining.streetdivider
+
+fun String.removeTrailingSpecialChars():String {
+    var result = this
+    for (ch in this.reversed()) {
+        if (!ch.isLetterOrDigit() && ch != '.') {
+            result = result.removeSuffix(ch.toString())
+        }
+    }
+    return result
+}
+
+
+fun String.standardizeLetters(): String {
+    var result = ""
+    for (ch in this) {
+        if (ch.isLetterOrDigit()) {
+            val lowerCh = ch.toLowerCase()
+            result += when (lowerCh) {
+                'ä' -> "ae"
+                'ö' -> "oe"
+                'ü' -> "ue"
+                'ß' -> "ss"
+                else -> lowerCh.toString()
+            }
+        }
+    }
+    return result
+}
+
+fun String.standardizeStreetSuffix(): String {
+    if (this.length < 6) return this
+    return when (this.substring(this.length - 6)) {
+        "Straße" -> this.substring(0, this.length - 6) + "Str."
+        "straße" -> this.substring(0, this.length - 6) + "str."
+        else -> this
+    }
+}
+
+fun String.standardizeStreetName(): String {
+    return this.standardizeStreetSuffix().standardizeLetters();
+}
+
+
+fun Char.isValidStreetSpecialChar(): Boolean {
+    return ".- ".contains(this);
+}
+
+
+
+fun main(args: Array<String>) {
+    println("Kultstraße 3".standardizeStreetSuffix().standardizeLetters())
+    println("Kultstraße".standardizeStreetSuffix().standardizeLetters())
+    println("straße 73".standardizeStreetSuffix().standardizeLetters())
+//    println("Garten12Weg".standardizeLetters())
+//    println(StreetDivider().parse("Heideweg2a"))
+//    println(StreetDivider().parse("M1, Nr. 3"))
+//    println(StreetDivider().parse("1. Wasserstro 3b"))
+}
