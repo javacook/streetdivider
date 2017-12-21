da8d50112caa696ebd126118da97050f9f8c39b2
Joerg Vollmer <J.Vollmer@verband.creditreform.de>
2017-12-21 16:14:01 +0100

    Bugfix

--- src/main/kotlin/StreetReader.kt
+++ src/main/kotlin/StreetReader.kt
@@ -7,7 +7,7 @@ object StreetReader {
 
     init {
         val resource = this::class.java.classLoader.getResource("specialstreets.txt")
-        val readText = resource.readText(Charset.defaultCharset())
+        val readText = resource.readText(Charset.forName("UTF-8"))
         val lines = readText.split(Regex("""\r\n""")).map({it.trim()})
         if (lines.size > 1) {
             streets = lines
@@ -19,7 +19,10 @@ object StreetReader {
             }
             else {
                 val lines = readText.split(Regex("""\r""")).map({it.trim()})
-                streets = lines
+                if (lines.size > 1) {
+                    streets = lines
+                }
+                throw Exception("The file 'specialstreet.txt' could not be read correctly.")
             }
         }
     }
