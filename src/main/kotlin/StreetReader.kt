4888608e44bd7cb5ff1025d9c7a8830dce7a37de
javacook <javacook@gmx.de>
2017-12-22 21:41:45 +0100

    Better (generic) implementation

--- src/main/kotlin/StreetReader.kt
+++ src/main/kotlin/StreetReader.kt
@@ -2,28 +2,25 @@ package de.kotlincook.textmining.streetdivider
 
 import java.nio.charset.Charset
 
+/**
+ * Reads the special streets from the file <code>specialstreets.txt</code>. It
+ * can be accessed via the public property <code>sreeet</code>
+ */
 object StreetReader {
     val streets: List<String>
 
     init {
         val resource = this::class.java.classLoader.getResource("specialstreets.txt")
         val readText = resource.readText(Charset.forName("UTF-8"))
-        val lines = readText.split(Regex("""\r\n""")).map({it.trim()})
-        if (lines.size > 1) {
-            streets = lines
+        var lines: List<String> = arrayListOf()
+        // Try all line separator types...
+        for (lineSeparator in arrayOf("""\r\n""", """\n""", """\r""")) {
+            lines = readText.split(Regex(lineSeparator)).map({it.trim()})
+            if (lines.size > 1) break
         }
-        else {
-            val lines = readText.split(Regex("""\n""")).map({it.trim()})
-            if (lines.size > 1) {
-                streets = lines
-            }
-            else {
-                val lines = readText.split(Regex("""\r""")).map({it.trim()})
-                if (lines.size > 1) {
-                    streets = lines
-                }
-                throw Exception("The file 'specialstreet.txt' could not be read correctly.")
-            }
+        streets = lines
+        if (lines.size <= 1) {
+            throw Exception("The file 'specialstreet.txt' could not be read correctly.")
         }
     }
 }
