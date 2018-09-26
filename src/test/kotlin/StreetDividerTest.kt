87d1360739888b48f24321f0e3f403ca3e7d3e1c
javacook <javacook@gmx.de>
2018-09-26 23:34:23 +0200

    Zeitmessung mit measureTimeMillis

--- src/test/kotlin/StreetDividerTest.kt
+++ src/test/kotlin/StreetDividerTest.kt
@@ -2,26 +2,27 @@ package de.kotlincook.textmining.streetdivider
 
 import org.junit.Test
 import kotlin.math.absoluteValue
+import kotlin.system.measureTimeMillis
 
 
 class StreetDividerTest {
-    
+
     @Test
     fun checkRandomlyCreatedStreetnamesForExceptions() {
-        val startTime = System.currentTimeMillis()
-        val streetDivider = StreetDivider()
-        for (cnt in 0..100){
-            for (len in 0..50) {
-                var input = ""
-                for (i in 0 until len) {
-                    val zahl: Int = (Math.random() * 256).toInt()
-                    input += (zahl.absoluteValue % 256).toChar();
+        val time = measureTimeMillis {
+            val streetDivider = StreetDivider()
+            for (cnt in 0..100) {
+                for (len in 0..50) {
+                    var input = ""
+                    for (i in 0 until len) {
+                        val zahl: Int = (Math.random() * 256).toInt()
+                        input += (zahl.absoluteValue % 256).toChar();
+                    }
+                    streetDivider.parse(input)
                 }
-                streetDivider.parse(input)
             }
         }
-        val endTime = System.currentTimeMillis()
-        println("Execution time ${endTime - startTime} ms")
+        println("Execution time ${time} ms")
     }
 
 }
